f92f13acd690250ee56fd0b12872feb6950e2672
Daredevil <kaaan_s@yahoo.de>
2023-08-16 15:20:36 +0200

    - chapter 1.5 update

--- DailyMotivation/ContentView.swift
+++ DailyMotivation/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = "Yo"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack(spacing: 0) {
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .border(.orange, width: 1)
+                .padding()
+            
+            
+            HStack {
+                Button("Awesome") {
+                    messageString = "You Are Awesome!"
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                    
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .border(.purple, width: 5)
+            
+            
         }
-        .padding()
     }
 }
 
